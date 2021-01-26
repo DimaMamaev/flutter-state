ac9d826a2107d1d71e3d87c15765bf58937ce062
DimaMamaev <dmamaev@my.com>
2021-01-26 10:37:40 +0200

    creating products screen

--- lib/screens/products-list.dart/products-list.dart
+++ lib/screens/products-list.dart/products-list.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/products.dart';
 
 class ProductsListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shop'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 15,
+        ),
+        itemBuilder: (ctx, index) => Container(),
+        itemCount: productsList.length,
+      ),
+    );
   }
 }
