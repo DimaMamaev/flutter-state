c2b88f82d6fe83d28352d68102152a92e0241823
DimaMamaev <dmamaev@my.com>
2021-01-26 10:21:03 +0200

    added some data & models

---
+++ lib/models/products.dart
@@ -0,0 +1,35 @@
+import './product.dart';
+
+List<Product> productsList = [
+  Product(
+    id: 'p1',
+    title: 'Shirt',
+    description: 'Just a shirt',
+    price: 29.99,
+    imageUrl:
+        'https://www.all4o.com/image/cache/data/brand/TrueStory/TRUE-STORY-Elite-orienteering-shirt-Men-Deep-BLUE-800x800.jpg',
+  ),
+  Product(
+    id: 'p2',
+    title: 'Trousers',
+    description: 'A nice pair of trousers.',
+    price: 59.99,
+    imageUrl: 'https://content.moss.co.uk/images/original/966353577_01.jpg',
+  ),
+  Product(
+    id: 'p3',
+    title: 'Scarf',
+    description: 'Warm and cozy - exactly what you need for the winter.',
+    price: 19.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/8105zBthh7L._AC_UL1500_.jpg',
+  ),
+  Product(
+    id: 'p4',
+    title: 'A Pan',
+    description: 'Prepare any meal you want.',
+    price: 49.99,
+    imageUrl:
+        'https://www.schneider-gmbh.com/assets/produktbilder/531076-9f4e44ef315e685e75f59a6a251843f05b28d46ce7e281a295a7e2e29a8577e3.jpg',
+  ),
+];
